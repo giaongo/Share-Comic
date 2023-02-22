bda3eaca280dcffa4bc2d97e1ca69f475717a16d
Giao Ngo <giao.ngo@metropolia.fi>
2023-02-22 18:33:46 +0200

    tools: install craco, and other devDependencies. Downgrade the react-scripts and decode jwt token sent from google login

--- share-comic-frontend/src/container/Login.jsx
+++ share-comic-frontend/src/container/Login.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import bannerVideo from "../assets/video_banner.mp4";
 import appLogo from "../assets/logo.png";
+import { GoogleLogin } from "@react-oauth/google";
+const { OAuth2Client } = require("google-auth-library");
 
 const Login = () => {
+  const getDecodedOAuthJwtGoogle = async (token) => {
+    const CLIENT_ID_GOOGLE = process.env.REACT_APP_GOOGLE_CLIENTID;
+    try {
+      const client = new OAuth2Client(CLIENT_ID_GOOGLE);
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID_GOOGLE,
+      });
+      return ticket;
+    } catch (error) {
+      console.log("errorDecodingJwt", error);
+    }
+  };
   return (
     <div
       id="loginContainer"
@@ -19,11 +34,25 @@ const Login = () => {
         />
         <div className="absolute top-0 right-0 left-0 bottom-0 bg-pinkOverlay" />
       </div>
-      <div className="self-center absolute w-450 top-1/3 bg-loginContainer p-10 flex justify-center 
-      items-center rounded-lg drop-shadow-xl">
+      <div
+        className="self-center absolute w-450 top-1/4 bg-loginContainer p-10 flex justify-center 
+      items-center rounded-lg drop-shadow-xl"
+      >
         <div>
-          <img src={appLogo} width="130px" alt="logo"/>
-          <button className="w-full mt-5 bg-white p-5 rounded-lg">Login</button>
+          <img src={appLogo} width="210" alt="logo" className="mb-10" />
+          <GoogleLogin
+            theme="outline"
+            shape="circle"
+            onSuccess={async (credentialResponse) => {
+              const decodedResult = await getDecodedOAuthJwtGoogle(
+                credentialResponse.credential
+              );
+              console.log("result is", decodedResult);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
         </div>
       </div>
     </div>
--- share-comic-frontend/src/index.js
+++ share-comic-frontend/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "semantic-ui-css/semantic.min.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENTID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
